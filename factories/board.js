c7a3a07ae67de85b2281103297ba3d7770f29d99
David Kamayor <davidkamayor@gmail.com>
2023-05-12 09:04:21 -0400

    Add basic receiveAttack functionality

--- factories/board.js
+++ factories/board.js
@@ -1,3 +1,5 @@
+import Ship from "./ship";
+
 class Board {
   constructor(width, height, owner) {
     this.width = width;
@@ -6,9 +8,9 @@ class Board {
     this.board = [];
   }
 
-  createSquare(tileNumber, shipPresent = false, hit = false) {
+  createSquare(id, shipPresent = false, hit = false) {
     return {
-      tileNumber: tileNumber,
+      id: id,
       shipPresent: false,
       hit: false,
     };
@@ -18,6 +20,20 @@ class Board {
     let totalSquares = this.width * this.height;
     for (let i = 0; i < totalSquares; i++) {
       this.board.push(this.createSquare(i + 1));
+    }
+  }
+
+  receiveAttack(id) {
+    let tile = this.board[id - 1]; //should be shallow copy
+    if (tile.hit) {
+      return "You have already attacked this target!";
+    }
+    tile.hit = true;
+    console.log(this.board[id - 1]);
+    if (tile.shipPresent) {
+      //need to get shipname and call shipName.hit
+    } else {
+      return "Miss!";
     }
   }
 }
