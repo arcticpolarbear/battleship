f679419679f43364600e0ef23b6bf310ff456c1a
David Kamayor <davidkamayor@gmail.com>
2023-05-12 10:08:46 -0400

    Added tests for horizontal placement validity

--- tests/board.test.js
+++ tests/board.test.js
@@ -6,16 +6,25 @@ describe("board functions", () => {});
 
   beforeEach(() => {
     board = new Board(10, 10, "player");
+    board.fillBoard();
   });
 
   it("creates 100 squares", () => {
-    board.fillBoard();
     expect(board.board.length).toEqual(100);
   });
 
-  it("receiveAttack modify tile data", () => {
-    board.fillBoard();
+  it("receiveAttack modifies tile data", () => {
     board.receiveAttack(99);
     expect(board.board[99].hit).toEqual(true);
+  });
+
+  it("should reject bad horizontal placement", () => {
+    board.placeShip(8, 3, true);
+    expect(board.board[8].shipPresent).toEqual(false);
+  });
+  it("should accept good horizontal placement", () => {
+    board.placeShip(8, 2, true);
+    expect(board.board[8].shipPresent).toEqual(true);
+    expect(board.board[9].shipPresent).toEqual(true);
   });
 }
