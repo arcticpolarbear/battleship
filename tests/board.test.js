3b05f724b51dfabab1dfeb3b515666a068b3ac8f
David Kamayor <davidkamayor@gmail.com>
2023-05-12 08:31:35 -0400

    Add tests for tile generation

---
+++ tests/board.test.js
@@ -0,0 +1,25 @@
+import Board from "../factories/board";
+
+describe("board functions", () => {});
+{
+  let board;
+
+  beforeEach(() => {
+    board = new Board(10, 10, "player");
+  });
+
+  it("creates 100 squares", () => {
+    board.fillBoard();
+    expect(board.board.length).toEqual(100);
+  });
+
+  it("starts from index 1", () => {
+    board.fillBoard();
+    expect(board.board[0].id).toEqual(1);
+  });
+
+  it("ends at index 100", () => {
+    board.fillBoard();
+    expect(board.board[99].id).toEqual(100);
+  });
+}
