9bb348dc7b13b254edaf186e8f902587603fcd53
David Kamayor <davidkamayor@gmail.com>
2023-05-12 11:25:44 -0400

    improved tests for vertical placement

--- tests/board.test.js
+++ tests/board.test.js
@@ -27,19 +27,27 @@ describe("board functions", () => {});
     board.placeShip(8, 2, true);
     expect(board.board[8].shipPresent).toEqual(true);
     expect(board.board[9].shipPresent).toEqual(true);
+    board.placeShip(98, 2, true);
     expect(board.board[98].shipPresent).toEqual(true);
+    expect(board.board[99].shipPresent).toEqual(true);
   });
   it("should reject bad vertical placement", () => {
-    board.placeShip(8, 4, true);
-    expect(board.board[8].shipPresent).toEqual(false);
+    board.placeShip(8, 2, false);
+    expect(board.board[91].shipPresent).toEqual(false);
+    board.placeShip(99, 4, false);
     expect(board.board[99].shipPresent).toEqual(false);
     expect(board.board[98].shipPresent).toEqual(false);
     expect(board.board[75].shipPresent).toEqual(false);
   });
   it("should accept good vertical placement", () => {
-    board.placeShip(8, 2, true);
+    board.placeShip(55, 3, false);
+    console.log(board.placeShip(55, 3, false));
     expect(board.board[55].shipPresent).toEqual(true);
-    expect(board.board[9].shipPresent).toEqual(true);
+    expect(board.board[65].shipPresent).toEqual(true);
+    expect(board.board[75].shipPresent).toEqual(true);
+    expect(board.board[85].shipPresent).toEqual(false);
+    board.placeShip(88, 2, false);
     expect(board.board[88].shipPresent).toEqual(true);
+    expect(board.board[98].shipPresent).toEqual(true);
   });
 }
